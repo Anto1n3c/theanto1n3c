436a33f60d02adea69851ce660c1b3ff6a9cb898
Antoi1n3 <alakazam0876@proton.me>
2023-03-15 14:59:51 +0100

    clone repo

---
+++ src/utils/bin/index.ts
@@ -0,0 +1,4 @@
+export * from './about';
+export * from './theme';
+export * from './utils';
+export * from './weather';
---
+++ src/utils/bin/utils.ts
@@ -0,0 +1,45 @@
+import packageJson from '../../../package.json';
+import * as bin from './index';
+
+export const help = async (args: string[]): Promise<string> => {
+  const commands = Object.keys(bin).sort().join(', ');
+
+  return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
+};
+
+export const echo = async (args: string[]): Promise<string> => {
+  return args.join(' ');
+};
+
+
+export const date = async (args: string[]): Promise<string> => {
+  return new Date().toString();
+};
+
+
+export const sudo = async (args?: string[]): Promise<string> => {
+  setTimeout(function () {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  }, 1000);
+
+  return `Permission denied: unable to run the command '${args[0]}' as root.`;
+};
+
+
+export const banner = (args?: string[]): string => {
+  return `
+  ▄▄▄       ███▄    █ ▄▄▄█████▓ ▒█████   ██▓ ███▄    █ ▓█████ 
+  ▒████▄     ██ ▀█   █ ▓  ██▒ ▓▒▒██▒  ██▒▓██▒ ██ ▀█   █ ▓█   ▀ 
+  ▒██  ▀█▄  ▓██  ▀█ ██▒▒ ▓██░ ▒░▒██░  ██▒▒██▒▓██  ▀█ ██▒▒███   
+  ░██▄▄▄▄██ ▓██▒  ▐▌██▒░ ▓██▓ ░ ▒██   ██░░██░▓██▒  ▐▌██▒▒▓█  ▄ 
+   ▓█   ▓██▒▒██░   ▓██░  ▒██▒ ░ ░ ████▓▒░░██░▒██░   ▓██░░▒████▒
+   ▒▒   ▓▒█░░ ▒░   ▒ ▒   ▒ ░░   ░ ▒░▒░▒░ ░▓  ░ ▒░   ▒ ▒ ░░ ▒░ ░
+    ▒   ▒▒ ░░ ░░   ░ ▒░    ░      ░ ▒ ▒░  ▒ ░░ ░░   ░ ▒░ ░ ░  ░
+    ░   ▒      ░   ░ ░   ░      ░ ░ ░ ▒   ▒ ░   ░   ░ ░    ░   
+        ░  ░         ░              ░ ░   ░           ░    ░  ░
+                                                               v${packageJson.version}
+
+Type 'help' to see list of available commands.
+
+`;
+};
