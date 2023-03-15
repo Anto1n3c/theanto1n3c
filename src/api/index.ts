f5b526ff05a6ac6734432a2a9afc255bf8d34aeb
Antoi1n3 <alakazam0876@proton.me>
2023-03-15 18:23:54 +0100

    theme update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,6 @@
 import axios from 'axios';
 import config from '../../config.json';
 
-export const getProjects = async () => {
-  const { data } = await axios.get(
-    `https://api.github.com/users/${config.social.github}/repos`,
-  );
-
-  return data;
-};
-
 export const getBio = async () => {
   const { data } = await axios.get(config.bioUrl);
 
