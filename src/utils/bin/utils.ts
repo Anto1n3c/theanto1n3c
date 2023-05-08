5cb891fab0824509b1a3642728b8303b44640cd3
Antoi1n3 <alakazam0876@proton.me>
2023-05-09 00:15:42 +0200

    1.1

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -40,13 +40,13 @@ export const start = (args?: string[]): string => {
 
 Welcome to Antoine Crone 19.04.5 LTS (GNU/Linux 5.10.16.3-WSL2 x86_64)                        
   
- * Documentation:  https://help.ubuntu.com
+ * Root-me:        https://www.root-me.org/Anto1n3
  * Discord:        Anto1n3c#0261
  * Support:        antoine.crone@icloud.com
 
   System information as of ${new Date().toString()}
 
-  System name:  Antoine Crone      Processes:             Python/C++/Html/Css
+  System name:  Antoine Crone      Processes:             Python/C++/Html/Css/JS
   Usage of /:   18 years           Users logged in:       0
   Memory usage: 0%                 IPv4 address for eth0: 172.22.104.190
   Swap usage:   0%
